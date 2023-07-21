a67715bc43720ae27b3a12f6251c40390a0e7c6e
Carlos Martín <cmrbolsadevelopment@gmail.com>
2023-07-21 17:10:39 +0100

    Telegram

--- src/components/LandingSaberMas/LandingSaberMas.jsx
+++ src/components/LandingSaberMas/LandingSaberMas.jsx
@@ -9,7 +9,7 @@ const LandingSaberMas = () => {
     <div className="LandingSaberMas_quieres_saber_mas">
     <button className="LandingSaberMas_boton_quieres_saber_mas" onClick={handleButtonTelegramClick}>
       <div className="LandingSaberMas_texto">
-        <h1>¿Quieres saber más del taller?</h1>
+        <h1>¿Quieres saber más sobre el taller?</h1>
         <p>Entra en el <span>TELEGRAM</span> y pregunta todas tus dudas</p>
         <p>Únete</p>      
       </div>
