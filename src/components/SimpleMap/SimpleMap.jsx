6e3d6f79df410a181792c9d71683ac7e24189b6f
Carlos Martín <cmrbolsadevelopment@gmail.com>
2023-07-30 09:39:33 +0100

    icono

--- src/components/SimpleMap/SimpleMap.jsx
+++ src/components/SimpleMap/SimpleMap.jsx
@@ -1,4 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './SimpleMap.css'
 
@@ -6,18 +7,26 @@ const SimpleMap = () => {
     const posStart = [40.44795126357991, -3.6359747451386735]; 
     // const posEnd = [51.505, -0.07];
 
+    const customMarkerIcon = L.icon({
+        iconUrl: 'https://res.cloudinary.com/dwbxywvdw/image/upload/v1690705657/Iconos/mapa_l4hpan.png',
+        iconSize: [45, 45], // Ajusta el tamaño del ícono según tus necesidades
+        iconAnchor: [16, 32], // Ajusta el punto de anclaje si es necesario
+        popupAnchor: [0, -32], // Ajusta la posición del popup con respecto al ícono
+      });
+
     return (
         <div className="simple-map-container">
-            <MapContainer center={posStart} zoom={13} style={{ height: '100%', width: '100%' }}>
+            <MapContainer center={posStart} zoom={18} style={{ height: '100%', width: '100%' }}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='© <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
                 />
-                <Marker position={posStart}>
+                <Marker position={posStart} icon={customMarkerIcon}>
                     <Popup>
                         Hotel Silken Puerta de Madrid 🏩
                     </Popup>
                 </Marker>
+
             </MapContainer>
         </div>
     );
